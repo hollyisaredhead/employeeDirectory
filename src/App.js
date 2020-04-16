fd439ccb23063faf1fd8d0aa77fd75dab6db4e4d
Holly Phelps <Hollyphelps1992@gmail.com>
2020-04-15 22:51:56 -0700

    directory created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
-import Form from "./components/Form";
+import Main from "./components/Main";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+import "./App.css";
 
 function App() {
-  return <Form />;
+  return (
+    <div className="App">
+      <Wrapper>
+        <Header />
+        <Main />
+      </Wrapper>
+    </div>
+  );
 }
 
 export default App;
